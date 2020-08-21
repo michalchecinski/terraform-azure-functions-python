07282d01288cc8b94480ff7f86015a86fee86aa4
Michał Chęciński <michal.checinski@outlook.com>
2020-08-21 10:35:13 +0200

    Fix storage conn string to name and access key

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -36,11 +36,12 @@ resource "azurerm_app_service_plan" "functions" {
 }
 
 resource "azurerm_function_app" "functions" {
-  name                      = var.function_app_name
-  location                  = azurerm_resource_group.rg.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  app_service_plan_id       = azurerm_app_service_plan.functions.id
-  storage_connection_string = azurerm_storage_account.functions.primary_connection_string
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.functions.id
+  storage_account_name       = azurerm_storage_account.functions.name
+  storage_account_access_key = azurerm_storage_account.functions.primary_access_key
 
   version = "~2"
 
