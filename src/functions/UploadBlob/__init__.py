4352ba8820dde84a42c468d6df5e327a927a817d
Michał Chęciński <michalchecinski@users.noreply.github.com>
2020-09-01 22:38:54 +0200

    Fix function (#13)
    * Fix function
    
    * Delete hash

--- src/functions/UploadBlob/__init__.py
+++ src/functions/UploadBlob/__init__.py
@@ -14,19 +14,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if files_no == 0:
         return func.HttpResponse(f"There are no files in request.", status_code=500)
 
-    storage_name = os.environ["storage_name"]
+    storage_name = "stfilesmch01" # os.environ["storage_name"]
     account_url = f"https://{storage_name}.blob.core.windows.net"
 
     credential = DefaultAzureCredential()
 
     blob_service_client = BlobServiceClient(account_url, credential)
 
-    container_name = "quickstart"
+    container_name = "http_files"
 
     blob_service_client.get_container_client(container_name)
 
-    for file in files:
+    for file_key in files:
+        file = files[file_key]
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=file.filename)
-        blob_client.upload_blob(str(file))
+        blob_client.upload_blob(file.stream)
 
-    return func.HttpResponse(f"There are {files_no} files in request.")
+    return func.HttpResponse(f"Uploaded {files_no} files from request.")
